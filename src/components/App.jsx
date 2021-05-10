bd40d808d793d4021eac4d0d5ee85a0502965b1c
hemanth253 <83533561+hemanth253@users.noreply.github.com>
2021-05-10 18:25:18 +0530

    v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function bothInputHandler(event) {
+    // Even this works
+    // const value = event.target.value;
+    // const name = event.target.name;
+
+    const { value, name } = event.target;
+
+    // This event is a Synthetic one not real so never use it in the below callback function
+    setFullName((prevValue) => {
+      if (name == "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        };
+      } else if (name == "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={bothInputHandler}
+          name="fName"
+          value={fullName.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={bothInputHandler}
+          name="lName"
+          value={fullName.lName}
+          placeholder="Last Name"
+        />
         <button>Submit</button>
       </form>
     </div>
